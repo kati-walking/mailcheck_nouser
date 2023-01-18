2a00e9ac88983625d1a9618ec54043ba65b4e2da
kati-walking <67584979+kati-walking@users.noreply.github.com>
2023-01-18 10:12:54 +0900

    add path parameter Maillists

--- mailcheck_nouser_front/src/Router.tsx
+++ mailcheck_nouser_front/src/Router.tsx
@@ -10,7 +10,7 @@ export default function Router(){
             <Routes>
                 <Route path = "/Userpage" element = {<Userpage />} />
                 <Route path = "/Regist" element={<RegistAdress />} />
-                <Route path = "/Maillists" element = {<Maillists />} />
+                <Route path = "/Maillists/:date" element = {<Maillists />} />
             </Routes>
         </BrowserRouter>
     )
